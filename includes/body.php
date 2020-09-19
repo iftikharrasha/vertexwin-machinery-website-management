c09b7e362ed5cceda6753e37b3f96de6b6c1ebfa
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-09-19 11:41:24 +0600

    banner section added with bootstrap slider

---
+++ includes/body.php
@@ -0,0 +1,56 @@
+
+<div class="ad" style="left: 0px;height: auto !important;">
+	   <!-- <script
+		<span>a d v e r t i s e m e n t</span>
+		</script>-->
+		<img src="resource/img/unnamed.png" alt="" style="width: 100%;">
+</div>
+
+<div class="ad" style="right: 0px; height: auto !important;">
+        <!-- <script
+		<span>a d v e r t i s e m e n t</span>
+		</script>-->
+		<img src="resource/img/unnamed.png" alt="" style="width: 100%;">
+</div>
+
+<div class="content content-upped">
+       <div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+				   
+					<!-- Wrapper for slides //banners-->
+					<div class="carousel-inner">
+
+					  <div class="item active">
+						<img src="resource/img/machine-banner.jpg" alt="Los Angeles" style="width:100%;">
+					  </div>
+
+					  <div class="item">
+						<img src="resource/img/heavy-banner.jpg" style="width:100%;">
+					  </div>
+					
+					  <div class="item">
+						<img src="resource/img/industrial-banner.jpg" alt="New York" style="width:100%;">
+					  </div>
+					  <div class="item">
+						<img src="resource/img/easy-banner.jpg" alt="New York" style="width:100%;">
+					  </div>
+				  
+					</div>
+
+					<!-- Left and right controls -->
+					<a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+					  <span class="glyphicon glyphicon-chevron-left"></span>
+					  <span class="sr-only" >Previous</span>
+					</a>
+					<a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+					  <span class="glyphicon glyphicon-chevron-right"></span>
+					  <span class="sr-only">Next</span>
+					</a>
+				</div>
+		</div>
+	</div>
+</div>
+</div>
+		
