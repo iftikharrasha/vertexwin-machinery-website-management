7590f285d45e6a2fc41f489c3fc4cd8ccc574776
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-09-21 10:10:49 +0600

    featured item section added

--- includes/body.php
+++ includes/body.php
@@ -1,56 +1,163 @@
 
 <div class="ad" style="left: 0px;height: auto !important;">
-	   <!-- <script
-		<span>a d v e r t i s e m e n t</span>
+	   <!-- <script><span>a d v e r t i s e m e n t</span>
 		</script>-->
-		<img src="resource/img/unnamed.png" alt="" style="width: 100%;">
+		<img src="resource/img/unnamed.png" alt="ad" style="width: 100%;">
 </div>
 
 <div class="ad" style="right: 0px; height: auto !important;">
-        <!-- <script
-		<span>a d v e r t i s e m e n t</span>
+        <!-- <script><span>a d v e r t i s e m e n t</span>
 		</script>-->
-		<img src="resource/img/unnamed.png" alt="" style="width: 100%;">
+		<img src="resource/img/unnamed.png" alt="ad" style="width: 100%;">
 </div>
 
 <div class="content content-upped">
-       <div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-				   
-					<!-- Wrapper for slides //banners-->
-					<div class="carousel-inner">
+		
+		<section>
+        <!-- container -->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+					   
+						   <!-- Wrapper for banners-->
+						   <div class="carousel-inner">
 
-					  <div class="item active">
-						<img src="resource/img/machine-banner.jpg" alt="Los Angeles" style="width:100%;">
-					  </div>
+							  <div class="item active">
+								<img src="resource/img/machine-banner.jpg" alt="machine-banner" style="width:100%;">
+							  </div>
 
-					  <div class="item">
-						<img src="resource/img/heavy-banner.jpg" style="width:100%;">
-					  </div>
-					
-					  <div class="item">
-						<img src="resource/img/industrial-banner.jpg" alt="New York" style="width:100%;">
-					  </div>
-					  <div class="item">
-						<img src="resource/img/easy-banner.jpg" alt="New York" style="width:100%;">
-					  </div>
-				  
+							  <div class="item">
+								<img src="resource/img/heavy-banner.jpg" alt="heavy-banner" style="width:100%;">
+							  </div>
+							
+							  <div class="item">
+								<img src="resource/img/industrial-banner.jpg" alt="industrial-banner" style="width:100%;">
+							  </div>
+							  <div class="item">
+								<img src="resource/img/easy-banner.jpg" alt="easy-banner" style="width:100%;">
+							  </div>
+						  
+							</div>
+
+							<!-- Left and right controls -->
+							<a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+							  <span class="glyphicon glyphicon-chevron-left"></span>
+							  <span class="sr-only" >Previous</span>
+							</a>
+							<a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+							  <span class="glyphicon glyphicon-chevron-right"></span>
+							  <span class="sr-only">Next</span>
+							</a>
 					</div>
-
-					<!-- Left and right controls -->
-					<a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
-					  <span class="glyphicon glyphicon-chevron-left"></span>
-					  <span class="sr-only" >Previous</span>
-					</a>
-					<a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
-					  <span class="glyphicon glyphicon-chevron-right"></span>
-					  <span class="sr-only">Next</span>
-					</a>
 				</div>
-		</div>
-	</div>
-</div>
+			 </div>
+		  </div>
+		  <!-- container -->
+		  </section>
+		  
+		  <section>
+		  <!-- container -->
+		  <div class="container">
+				<div class="row">
+					<!-- featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=72">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/textilecomponents.jpg" alt="textile components">
+								</div>
+								<div class="featured-body">
+									<h3>Textile<br>Components</h3>
+									<a href="product.php?p=72" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+						</a>
+					</div>
+					<!-- /featured -->
+					
+					<!-- /featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=78">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/spareparts.jpg" alt="spare parts">
+								</div>
+								<div class="featured-body">
+									<h3>Spare<br>Parts</h3>
+									<a href="product.php?p=78" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+					    </a>
+					</div>
+					<!-- /featured -->
+
+					<!-- featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=79">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/labequipments.jpg" alt="lab equipments">
+								</div>
+								<div class="featured-body">
+									<h3>Lab<br>Equipments</h3>
+									<a href="product.php?p=79" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+						</a>
+					</div>
+					<!-- /featured -->
+				
+					<!-- featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=72">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/bobbintool.jpg" alt="bobbin tool">
+								</div>
+								<div class="featured-body">
+									<h3>Bobbin<br>Tool</h3>
+									<a href="product.php?p=72" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+						</a>
+					</div>
+					<!-- /featured -->
+					
+					<!-- /featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=78">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/spindletape.jpg" alt="spindle tape">
+								</div>
+								<div class="featured-body">
+									<h3>Spindle<br>Tape</h3>
+									<a href="product.php?p=78" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+					    </a>
+					</div>
+					<!-- /featured -->
+
+					<!-- featured -->
+					<div class="col-lg-4 col-sm-6 col-xs-12">
+						<a href="product.php?p=79">
+							<div class="featured">
+								<div class="featured-img">
+									<img src="resource/img/ballbearing.jpg" alt="ball bearing">
+								</div>
+								<div class="featured-body">
+									<h3>Ball<br>Bearing</h3>
+									<a href="product.php?p=79" class="cta-btn">Order now <i class="fa fa-arrow-circle-right"></i></a>
+								</div>
+							</div>
+						</a>
+					</div>
+					<!-- /featured -->
+				</div>
+		  </div>
+		  <!-- /container -->
+		  </section>
 </div>
 		
