3c85cb9eb4c245beb7322772993d43ba7bc4c8cb
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-09-27 12:07:22 +0600

    Top selling products section added

--- includes/body.php
+++ includes/body.php
@@ -417,5 +417,199 @@
 					</div>
 				</div>
 		  </section>
+		  
+		  <section class="section">
+		  <div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="section-title">
+							<h3>Top selling</h3>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12">
+					<div class="products-tabs">
+						<!-- tab -->
+						<div id="tab2" class="tab-pane active">
+							<!-- products-slick -->
+							<div class="products-slick" data-nav="#slick-nav-2">
+							   <!-- products -->
+								<div class="product">
+									<a href="#">
+									  <div class="product-img">
+											<img src="resource/img/bobbintool.jpg" style="max-height: 170px;" alt="">
+											<div class="product-label">
+											  <span class="new">TOP</span>
+											</div>
+									  </div>
+									</a>
+									<div class="product-body">
+										  <p class="product-category">Gadgets
+										  </p>
+										  <h3 class="product-name header-cart-item-name">
+											<a
+											   href="#">sony Headphones
+											</a>
+										  </h3>
+										  <div class="product-rating">
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+										  </div>
+									</div>
+								 </div>
+								 <!-- products -->
+								<!-- products -->
+								<div class="product">
+									<a href="#">
+									  <div class="product-img">
+											<img src="resource/img/ballbearing.jpg" style="max-height: 170px;" alt="">
+											<div class="product-label">
+											  <span class="new">TOP</span>
+											</div>
+									  </div>
+									</a>
+									<div class="product-body">
+										  <p class="product-category">Gadgets
+										  </p>
+										  <h3 class="product-name header-cart-item-name">
+											<a
+											   href="#">sony Headphones
+											</a>
+										  </h3>
+										  <div class="product-rating">
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+										  </div>
+									</div>
+								 </div>
+								 <!-- products -->
+								 
+								 <!-- products -->
+								<div class="product">
+									<a href="#">
+									  <div class="product-img">
+											<img src="resource/img/textilecomponents.jpg" style="max-height: 170px;" alt="">
+											<div class="product-label">
+											  <span class="new">TOP</span>
+											</div>
+									  </div>
+									</a>
+									<div class="product-body">
+										  <p class="product-category">Gadgets
+										  </p>
+										  <h3 class="product-name header-cart-item-name">
+											<a
+											   href="#">sony Headphones
+											</a>
+										  </h3>
+										  <div class="product-rating">
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+										  </div>
+									</div>
+								 </div>
+								 <!-- products -->
+								 
+								 <!-- products -->
+								<div class="product">
+									<a href="#">
+									  <div class="product-img">
+											<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+											<div class="product-label">
+											  <span class="new">TOP</span>
+											</div>
+									  </div>
+									</a>
+									<div class="product-body">
+										  <p class="product-category">Gadgets
+										  </p>
+										  <h3 class="product-name header-cart-item-name">
+											<a
+											   href="#">sony Headphones
+											</a>
+										  </h3>
+										  <div class="product-rating">
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+										  </div>
+									</div>
+								 </div>
+								 <!-- products -->
+								 
+								 <!-- products -->
+								<div class="product">
+									<a href="#">
+									  <div class="product-img">
+											<img src="resource/img/spareparts.jpg" style="max-height: 170px;" alt="">
+											<div class="product-label">
+											  <span class="new">TOP</span>
+											</div>
+									  </div>
+									</a>
+									<div class="product-body">
+										  <p class="product-category">Gadgets
+										  </p>
+										  <h3 class="product-name header-cart-item-name">
+											<a
+											   href="#">sony Headphones
+											</a>
+										  </h3>
+										  <div class="product-rating">
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+											<i class="fa fa-star">
+											</i>
+										  </div>
+									</div>
+								 </div>
+								 <!-- products -->
+								 
+							</div>
+							<!-- products-slick -->
+							<div id="slick-nav-2" class="products-slick-nav"></div>
+						</div>
+						<!-- tab -->
+					</div>
+				</div>
+		  </div>
+		  </section>
 </div>
 		
