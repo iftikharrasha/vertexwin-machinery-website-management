4a44e17a78f3fbbf94110f1a2e48f51ae3893a85
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-10-03 13:17:49 +0600

    product page has been completed

--- products.php
+++ products.php
@@ -11,28 +11,321 @@ include "includes/advertisements.php";
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3">
+					<!-- Categories -->
 					<div class="aside">
 						<h3 class="aside-title">Categories</h3>
 						<div class="btn-group-vertical">
 							<div type="button" class="btn navbar-btn category">
 									<a href="#">Bobbin<small class="qty">(5)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Spare<small class="qty">(7)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Reeds<small class="qty">(4)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Caster<small class="qty">(2)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Balloon<small class="qty">(8)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Rubber<small class="qty">(5)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Bearing<small class="qty">(3)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Snail Wire<small class="qty">(3)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Unicutter<small class="qty">(5)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Expander<small class="qty">(7)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Tester/Length Meter<small class="qty">(8)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
 									<a href="#">Other<small class="qty">(7)</small></a>
 							</div>
 						</div>
 					</div>
+					<!-- Categories -->
+					
+					<!-- Countries -->
+					<div class="aside">
+						<h3 class="aside-title">Countries</h3>
+						<div class="btn-group-vertical">
+							<div type="button" class="btn navbar-btn category">
+									<a href="#">Japan<small class="qty">(5)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
+									<a href="#">Korea<small class="qty">(7)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
+									<a href="#">China<small class="qty">(4)</small></a>
+							</div>
+							<div type="button" class="btn navbar-btn category">
+									<a href="#">India<small class="qty">(2)</small></a>
+							</div>
+						</div>
+					</div>
+					<!-- Countries -->
 				</div>
 				<div class="col-md-9">
-				
+					<!-- Products -->
+					<div class="get_product">
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+						
+						<!-- Item -->
+						<div class="col-md-4 col-xs-6">
+							<div class="product">
+								<a href="product.php?p=1">
+									<div class="product-img">
+										<img src="resource/img/spindletape.jpg" style="max-height: 170px;" alt="">
+										<div class="product-label">
+											<span class="new">NEW</span>
+										</div>
+									</div>
+								</a>
+								<div class="product-body">
+									<p class="product-category">Bobbin</p>
+									<h3 class="product-name header-cart-item-name"><a href="product.php?p=1">Demo Title 1</a></h3>
+									<div class="product-rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Item -->
+					</div>
+					<!-- Products -->
+					
+					<div class="product-filter clearfix">
+						<div class="pad"><span class="product-qty">Showing 1-9 of all products</span></div>
+						<ul class="product-pagi" id="pageno">
+							<li><a href="#product-row" page="1" id="page" class="active">1</a></li>
+							<li><a href="#product-row" page="2" id="page" class="active">2</a></li>
+							<li><a href="#product-row" page="3" id="page" class="active">3</a></li>
+							<li><a href="#product-row" page="4" id="page" class="active">4</a></li>
+							<li><a href="#product-row" page="5" id="page" class="active">5</a></li>
+							<li><a href="#product-row" page="6" id="page" class="active">6</a></li>
+							<li><a href="#product-row" page="7" id="page" class="active">7</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
