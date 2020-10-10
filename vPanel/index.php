1f3c3cb889b23c48d27dbafe187cc3b1f8afd69b
Iftikhar Rasha <iftikhar.rasha@northsouth.edu>
2020-10-10 20:46:20 +0600

    admin login page added

---
+++ vPanel/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vPanel</title>
+
+    <!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css"/>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+
+    <!-- Custom stlylesheet -->
+    <link type="text/css" rel="stylesheet" href="../css/style.css"/>
+    <link type="text/css" rel="stylesheet" href="../css/media.css"/>   
+</head>
+<body>
+<div>
+    <div class="login_wrapper">
+        <div class="login_form">
+            <section class="login_content">
+                <form action="#" method="post">
+                    <h1>Log in to your vPanel</h1>
+                    <div>
+                        <input type="email" id="userName" name="userName" class="form-control" placeholder="Your Email" required="">
+                    </div>
+                    <div>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
+                    </div>
+                    <div>
+                        <button type="submit" name="login" class="btn btn-default submit">Log in</button>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="bridge">
+                        <div>
+                            <h1><i class="fa fa-asterisk"></i> Vertex Win!</h1>
+                            Developed by <a href="#iftikharrasha" target="_blank">Iftikhar Rasha</a>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </div>
+</div>
+</body>
+</html>
